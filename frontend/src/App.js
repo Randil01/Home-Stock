f833a9380efa886c26f5977dc7c8c71973851cff
Randil22 <thenularandila2002@gmail.com>
2025-04-02 23:43:11 +0530

    bug fixing
    Fixed bugs in node modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import UpdateRemind from './component/reminds/updateRemind';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import ProtectedRoute from './components/ProtectedRoute';
-
 // Import Budget
 import AddBudget from './component/budget/addBudget';
 import BudgetReport from './component/budget/displayBudget';
@@ -25,47 +24,15 @@ function App() {
         <Route path="/signup" element={<Signup />} />
 
         {/* Protected routes */}
-        <Route path="/" element={
-          <ProtectedRoute>
-            <DisplayRemind />
-          </ProtectedRoute>
-        } />
-        <Route path="/add" element={
-          <ProtectedRoute>
-            <AddRemind />
-          </ProtectedRoute>
-        } />
-        <Route path="/updateremind/:id" element={
-          <ProtectedRoute>
-            <UpdateRemind />
-          </ProtectedRoute>
-        } />
-        <Route path="/addBudget" element={
-          <ProtectedRoute>
-            <AddBudget />
-          </ProtectedRoute>
-        } />
-        <Route path="/budgetDisplay" element={
-          <ProtectedRoute>
-            <BudgetReport />
-          </ProtectedRoute>
-        } />
-        <Route path="/invform" element={
-          <ProtectedRoute>
-            <InventoryForm />
-          </ProtectedRoute>
-        } />
-        <Route path="/invlist" element={
-          <ProtectedRoute>
-            <InventoryList />
-          </ProtectedRoute>
-        } />
-        <Route path="/invreport" element={
-          <ProtectedRoute>
-            <Report />
-          </ProtectedRoute>
-        } />
-          <Route path="/home" element={<Home/>} />
+        <Route path="/home" element={<ProtectedRoute><Home/> </ProtectedRoute>} />
+        <Route path="/" element={<ProtectedRoute><DisplayRemind /> </ProtectedRoute> } />
+        <Route path="/add" element={ <ProtectedRoute><AddRemind /> </ProtectedRoute> } />
+        <Route path="/updateremind/:id" element={<ProtectedRoute><UpdateRemind /> </ProtectedRoute>} /> 
+        <Route path="/addBudget" element={ <ProtectedRoute> <AddBudget /> </ProtectedRoute> } />
+        <Route path="/budgetDisplay" element={ <ProtectedRoute><BudgetReport /> </ProtectedRoute>} />
+        <Route path="/invform" element={ <ProtectedRoute><InventoryForm /> </ProtectedRoute>} />
+        <Route path="/invlist" element={ <ProtectedRoute> <InventoryList /> </ProtectedRoute> } />
+        <Route path="/invreport" element={ <ProtectedRoute><Report /> </ProtectedRoute> } />
       </Routes>
     </div>
   );

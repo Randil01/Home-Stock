fde41c5a66e4aef1efd9606ca0ee24052d3bfe00
Randil22 <thenularandila2002@gmail.com>
2025-04-02 11:32:25 +0530

    Smart restocking
    Function 100% completed. checked the mailing cabibilties.

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,9 @@ mongoose.connect("mongodb+srv://thenularandila2002:Thenula2002@cluster0.tzu8j.mo
 const reminds = require("./routes/remindRoutes");
 const number = require("./routes/emailRoutes");
 const inventoryRoutes = require('./routes/inventoryRoutes');
+const check = require('./routes/restockroute')
 
 app.use("/notification", reminds);
 app.use("/email",number);
 app.use('/api/inventory', inventoryRoutes);
+app.use('/checkRestock',check);
